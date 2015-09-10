d6ae185a79123f8098749a768f17ed7e1744ea7e
itsmoops <moore.ericc@gmail.com>
2015-09-10 18:53:34 +0200

    Initial Test Commit

---
+++ src/app/app.spec.js
@@ -0,0 +1,9 @@
+describe( 'jasmine tests', function() {
+ it('should work', function(){
+   expect(true).toBe(true);
+ });
+ it('should have a not case', function(){
+   expect(true).not.toBe(false);
+ });
+});
+ 
