c33ab639bd9d268b7fd79bc67515c1acb0759816
itsmoops <moore.ericc@gmail.com>
2015-09-25 11:34:39 -0500

    updating github

---
+++ build/src/app/config.js
@@ -0,0 +1,13 @@
+angular.module('app.config', [])
+
+.constant('ENV', 'development')
+
+.constant('DEV_MODE', true)
+
+.constant('API_DOMAIN', 'http://localhost:8080/')
+
+.constant('LOCAL_DOMAIN', 'localhost:8000/#/')
+
+.constant('LOCAL_PROTOCOL', 'http')
+
+;
